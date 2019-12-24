05eaba1757d32eea9bd59f818cdfc1ade3d74b14
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2019-12-24 21:14:25 +0530

    BT inorder Build And Print

---
+++ BTinorderBuildAndPrint.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+    node * root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void printIn(node*root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise Left Root Right
+    printIn(root->left);
+    cout<<root->data<<" ";
+    printIn(root->right);
+}
+
+
+int main(){ 
+    node* root = buildTree();
+    printIn(root);
+    cout<<endl;
+return 0;
+}
