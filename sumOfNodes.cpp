f208a2b1da5e783138d7da96ba0267acfbe8f80d
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2019-12-24 22:22:43 +0530

    Sum of nodes

---
+++ sumOfNodes.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+    node * root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+
+int sum(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    return root->data + sum(root->left) + sum(root->right);
+}
+
+int main(){ 
+    node* root = buildTree();
+    cout<<sum(root)<<endl;
+
+
+return 0;
+}
