2b4799578f127d7d58e4aa6f96df600545f1b689
Sanya Kumari <1701sanya@gmail.com>
2020-06-02 18:05:53 +0530

    Create BTpostorderBuildandPrint.cpp

---
+++ BTpostorderBuildandPrint.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+    node * root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void printPost(node*root){
+    if(root==NULL){
+        return;
+    }
+    printPost(root->left);
+    printPost(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main(){ 
+    node* root = buildTree();
+    printPost(root);
+    cout<<endl;
+return 0;
+}
